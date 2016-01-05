eb501ee9221c66aef26c2bc9fcb8751231603100
Ferrari Lee <shiyung@gmail.com>
2016-01-06 03:51:06 +0800

    webpack hello world example

---
+++ demo1/webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
+
+var TARGET = process.env.npm_lifecycle_event;
+var PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+var common = {
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack'
+    })
+  ]
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+} else if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
