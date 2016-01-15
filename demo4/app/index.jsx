a158a78ec9096bff2b0a811944022c1f02c164c6
Ferrari Lee <shiyung@gmail.com>
2016-01-15 21:30:09 +0800

    finish demo4 but not works yet

--- demo4/app/index.jsx
+++ demo4/app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './containers/Root'
+import Root from './containers/Root'
 import configureStore from './store/configureStore'
 
 const store = configureStore()
--- demo4/app/store/configureStore.prod.js
+++ demo4/app/store/configureStore.prod.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { reduxReactRouter } from 'redux-router'
 import createHistory from 'history/lib/createBrowserHistory'
 import routes from '../routes.js'
-import api from '../middlewares/api'
+import api from '../middleware/api'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
 
